9e571e9b1c22e2429a6ba29a0e08392b2b168a2f
Стив <kenethra.74@gmail.com>
2022-05-24 20:20:35 -0600

    feat: dialog validation errors

--- dash/mysql/mysql.go
+++ dash/mysql/mysql.go
@@ -4,9 +4,49 @@ import (
 	"adminmsyql/dash/adapter"
 	"adminmsyql/dash/models"
 	"database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Mysql struct {
+}
+
+const (
+	width = 96
+)
+
+var (
+	docStyle = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+
+	subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+
+	dialogBoxStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("#874BFD")).
+			Padding(1, 0).
+			BorderTop(true).
+			BorderLeft(true).
+			BorderRight(true).
+			BorderBottom(true)
+)
+
+func errorDialog(error string) {
+	doc := strings.Builder{}
+
+	errorMessage := lipgloss.NewStyle().Width(50).Align(lipgloss.Center).Render(error)
+	ui := lipgloss.JoinVertical(lipgloss.Center, errorMessage)
+	dialog := lipgloss.Place(width, 9,
+		lipgloss.Center, lipgloss.Center,
+		dialogBoxStyle.Render(ui),
+		lipgloss.WithWhitespaceChars("猫咪"),
+		lipgloss.WithWhitespaceForeground(subtle),
+	)
+
+	doc.WriteString(dialog + "\n\n")
+
+	fmt.Println(docStyle.Render(doc.String()))
 }
 
 func (client *Mysql) ListProfile() ([]models.Credential, error) {
@@ -17,14 +57,16 @@ func (client *Mysql) ListProfile() ([]models.Credential, error) {
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
 	rows, err := db.Query(`SELECT user, host, insert_priv, select_priv, update_priv, delete_priv, create_priv, drop_priv,
                                grant_priv, index_priv, alter_priv
                                FROM db where user not like '%mysql%'`)
+
 	if err != nil {
-		panic(err.Error())
+		errorDialog("127.0.0.1:3306: connect: connection refused")
 	}
+
+	defer db.Close()
 
 	for rows.Next() {
 		var (
